69ade2e3be804fcc4ed7cdf144c8e8c2279aeca4
Taisei <taisei96.web@gmail.com>
2023-12-15 08:30:42 +0900

    モーダル閉じる

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,97 @@
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import "./css/App.css";
+import "./css/modal.css"
 import { Kirokus } from "./types/Kirokus";
 import { Today } from "./types/Today";
-import Modal from "./components/modal/Modal";
+
 
 function App() {
   const kiroku1 = new Kirokus("1", "React", "Udemy入門", "3時間");
-  const kiroku2 = new Kirokus("2", "React", "Udemy入門", "3時間");
+  const today1 = new Today("1", "12.12", [kiroku1]);
 
-  const today1 = new Today("1", "12.12", [kiroku1, kiroku2]);
-  const today2 = new Today("2", "12.13", [kiroku1, kiroku2]);
+  /* useStateで記録（カテゴリ、内容、時間）を管理 */
+  const [kirokus, setKiroku] = useState([today1]);
 
-  // useStateで記録を管理
-  const [kirokus, setKiroku] = useState([today1, today2]);
+  /* useStateでinputに入力された値を管理 */
 
+  // 内容 //
+  const [inputCategory, setInputCategory] = useState("");
+  const [inputDetail, setInputDetail] = useState("");
+  const [inputTime, setInputTime] = useState("");
+
+  /* モーダル */
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const closeModal = useCallback(() => {
+    setIsModalOpen(false);
+  }, []);
+
+  useEffect(() => {
+    if (isModalOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "unset";
+    }
+  }, [isModalOpen]);
+
+  // 記録を一覧に登録する関数
+  const registerKiroku = () => {
+    const newKiroku = kirokus.map((kiroku) => {
+
+      return new Today(kiroku.id, kiroku.date, [ ...kiroku.memories, new Kirokus((Math.random() * 10000).toString(), inputCategory, inputDetail, inputTime) ]);
+    })
+
+    setKiroku(newKiroku)
+    setIsModalOpen(false)
+  };
+  /* モーダル */
+  
   return (
     <>
-      <Modal />
+      <button className="create-button" onClick={() => setIsModalOpen(true)}>
+        積み上げを記録する
+      </button>
+      {isModalOpen && (
+        <>
+          <div className="modal-bg"></div>
+          <div className="modal-box">
+            <p className="modal-form-title">積み上げを記録する</p>
+            <button className="close-button" onClick={closeModal}>
+              Close Modal
+            </button>
+            <div className="modal-form">
+              <div className="modal-label">
+                <label>カテゴリ</label>
+                <label>内容</label>
+                <label>時間</label>
+              </div>
+              <div className="modal-input-item">
+                <input
+                  type="text"
+                  onChange={(e) => setInputCategory(e.target.value)}
+                />
+                <input
+                  className="input-detail"
+                  type="text"
+                  value={inputDetail}
+                  onChange={(e) => setInputDetail(e.target.value)}
+                />
+                <input
+                  type="text"
+                  onChange={(e) => setInputTime(e.target.value)}
+                />
+              </div>
+              <div className="modal-button-wrapper">
+                <button className="button-base cancel-button" onClick={closeModal}>キャンセル</button>
+                <button className="button-base register-button" onClick={registerKiroku}>
+                  登録する
+                </button>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
+
       <ul className="kiroku-list">
         {kirokus.map((kiroku) => {
           return (
